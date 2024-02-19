bdab316535057a5164dc6aa3546dd095b993d755
ekawinata <eka.winata@gdn-commerce.com>
2024-02-19 20:50:06 +0700

    Add logging AOP

--- src/main/java/com/example/bliblispringdemo/aspect/CommandLoggingAspect.java
+++ src/main/java/com/example/bliblispringdemo/aspect/CommandLoggingAspect.java
@@ -3,8 +3,10 @@ package com.example.bliblispringdemo.aspect;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 import java.util.stream.Collectors;
@@ -15,18 +17,32 @@ import java.util.stream.Stream;
 @Component
 public class CommandLoggingAspect {
 
-  @Before("execution(* com.example.bliblispringdemo.command.Command.execute(..))")
+  @Pointcut("execution(* com.example.bliblispringdemo.command.Command.execute(..))")
+  public void executeCommandPointcut() {}
+
+  @Before("executeCommandPointcut()")
   public void logBeforeExecute(JoinPoint joinPoint) {
-    log.info("Executing %s with args: %s".formatted(
+    log.info("%s executing %s with args: %s".formatted(
+      getCommandName(joinPoint),
       joinPoint.getSignature().getName(),
       getCommandRequestString(joinPoint.getArgs())));
   }
 
-  @AfterReturning(pointcut = "execution(* com.example.bliblispringdemo.command.Command.execute(..))", returning = "result")
+  @AfterReturning(pointcut = "executeCommandPointcut()", returning = "result")
   public void logAfterExecute(JoinPoint joinPoint, Object result) {
-    log.info("Successfully executed %s with result: %s".formatted(
+    log.info("%s successfully executed %s with result: %s".formatted(
+      getCommandName(joinPoint),
       joinPoint.getSignature().getName(),
       result));
+  }
+
+  @AfterThrowing(pointcut = "executeCommandPointcut()", throwing = "throwable")
+  public void logAfterExecuteThrow(JoinPoint joinPoint, Throwable throwable) {
+    log.error("%s failed to executed with error:".formatted(getCommandName(joinPoint)), throwable.getMessage());
+  }
+
+  private String getCommandName(JoinPoint joinPoint) {
+    return "#%s".formatted(joinPoint.getTarget().getClass().getSimpleName());
   }
 
   private String getCommandRequestString(Object[] objects) {
