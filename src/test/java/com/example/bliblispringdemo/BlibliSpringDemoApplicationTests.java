cfa1096e8ed4b40164fdb784e0475ce4f763d1cf
ekawinata <eka.winata@gdn-commerce.com>
2024-02-19 21:52:56 +0700

    add test properties

--- src/test/java/com/example/bliblispringdemo/BlibliSpringDemoApplicationTests.java
+++ src/test/java/com/example/bliblispringdemo/BlibliSpringDemoApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.bliblispringdemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource("classpath:application-test.properties")
 class BlibliSpringDemoApplicationTests {
 
 	@Test
--- src/test/java/com/example/bliblispringdemo/controller/ParkingLotControllerTest.java
+++ src/test/java/com/example/bliblispringdemo/controller/ParkingLotControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -19,6 +20,7 @@ import java.util.Objects;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource("classpath:application-test.properties")
 public class ParkingLotControllerTest {
 
   @Autowired
